f5dde1be6c1e942c1487b89fddd9dcf355f4534f
Cesar J. Alaniz <cesar@alanisoft.com>
2014-12-12 11:12:32 -0800

    Update to golang 1.4

--- cookbooks/golang/attributes/default.rb
+++ cookbooks/golang/attributes/default.rb
@@ -1,4 +1,4 @@
-default['go']['version'] = '1.3.3'
+default['go']['version'] = '1.4'
 default['go']['platform'] = 'amd64'
 default['go']['filename'] = "go#{node['go']['version']}.#{node['os']}-#{node['go']['platform']}.tar.gz"
 default['go']['url'] = "https://storage.googleapis.com/golang/#{node['go']['filename']}"
